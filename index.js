81aea317bcbe6257585cca17b60876bd4f2ec46e
rayhan <rayhanmahmud990099@gmail.com>
2024-05-12 00:04:29 +0600

    done deleted

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, Collection, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lnrotgp.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,13 +26,14 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
 
-
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
 
         const menuCollection = client.db("bossDB").collection("menu");
         const reviewsCollection = client.db("bossDB").collection("reviews");
+        const cartCollection = client.db("bossDB").collection("carts");
+
 
 
         app.get('/menu', async (req, res) => {
@@ -45,6 +46,35 @@ async function run() {
             res.send(result)
         })
 
+
+        // carts collection 
+        app.get('/carts', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
+            const result = await cartCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.post('/carts', async (req, res) => {
+            const cartItem = req.body;
+            const result = await cartCollection.insertOne(cartItem);
+            res.send(result)
+        });
+
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id), }
+            const result = await cartCollection.deleteOne(query)
+            res.send(result)
+        })
+
+
+
+
+
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -56,10 +86,6 @@ async function run() {
 run().catch(console.dir);
 
 
-
-
-
-
 app.get('/', (req, res) => {
     res.send("boss is sitting")
 });
@@ -67,3 +93,16 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Bristo boss is sitting on port ${port}`)
 })
+
+/**
+ * --------------------------------
+ *         NAMING CONVENTION
+ * --------------------------------
+ * APP.GET('/users')
+ * APP.GET('/users/:id')
+ * APP.POST('/users')
+ * APP.PUT('/users/:id')
+ * APP.PATCH('/users/:id')
+ * APP.DELETE('/users/:id')
+ * 
+ */
